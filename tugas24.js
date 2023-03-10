07924900e523d0f74d1b2fcf8f5a08cbe8ac5f3a
rafi861 <muhammadrafi2545@gmail.com>
2023-03-11 06:54:24 +0700

    tugas 24 js

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+function tugas() {
+  var angka = 'angka sebelumnya : 32,33,16,40,9,7,11,2,44,1,66';
+  var angka1 = [32,33,16,40,9,7,11,2,44,1,66];
+  console.log( angka);
+  console.log(angka1.sort());
+  var angka2 = angka1.reverse()
+  console.log(angka2);
+}
+tugas()
